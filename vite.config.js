c948fdbb1cb10315188f9bd3485addf133cd9ec0
TimL135 <t.littschwager@mbd-team.de>
2023-06-16 08:24:26 +0000

    .

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig({build: {
-            outDir: "./docs",
-            assetsInlineLimit: 0
-          },
+export default defineConfig({
+    build: {
+        outDir: './docs',
+        assetsInlineLimit: 0,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.ts',
@@ -21,33 +22,5 @@ export default defineConfig({build: {
                 },
             },
         }),
-        ,
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.svg"],
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,svg,png,woff2}"],
-      },
-      manifest: {
-        background_color: "#ffffff",
-        theme_color: "#ffffff",
-        name: "PHHHP",
-        short_name: "PHHHP",
-        start_url: "/PHHHP/",
-        display: "standalone",
-        icons: [
-          {
-            src: "icon.png",
-            sizes: "256x256",
-            type: "image/png",
-            purpose: "any",
-          },
-        ],
-      },
-    }),
-  ],
-  server: {
-    host: true,
-  },
-  base: "./"
+    ],
 });
